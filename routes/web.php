57e145a40bcf42a55069cd812558e431b2dea8fd
alexsouchereau <W0632604@myscc.ca>
2023-04-14 13:59:20 -0400

    Replaced welcome page with dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::resource('groups', 'App\Http\Controllers\GroupController');
