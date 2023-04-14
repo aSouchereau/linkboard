f160987e126bc8d2f5812cec1beae6229dfd073d
alexsouchereau <W0632604@myscc.ca>
2023-04-14 14:08:07 -0400

    Created master page and extended it on dashboard

---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Linkboard - Home</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-light"></nav>
+    </header>
+    <main class="container-fluid">
+                @yield('content')
+    </main>
+    <footer></footer>
+</body>
+</html>
