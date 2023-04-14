66dbb170dae2e332a49eef8f061db30c79cb551e
alexsouchereau <W0632604@myscc.ca>
2023-04-14 16:43:51 -0400

    Added section for user created groups

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
+        $user = User::findOrFail(Auth::id());
         $groups = Group::all();
-        return view('dashboard', compact('groups'));
+        $userGroups = $user->groups;
+        return view('dashboard', compact('groups', 'userGroups'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,4 +4,11 @@
     @foreach($groups as $group)
         <x-link-list groupId="{{$group->id}}"></x-link-list>
     @endforeach
+
+    <section class="container-fluid">
+        <h2>User Groups</h2>
+    @foreach($userGroups as $group)
+        <x-link-list groupId="{{$group->id}}"></x-link-list>
+    @endforeach
+    </section>
 @endsection
